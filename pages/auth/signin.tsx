06fe8a767e5f4679170595ba5f4feff85c88b0a9
Hugo Tamayo <doctamayot@hotmail.com>
2022-09-17 20:49:57 -0500

    Next auth basico

---
+++ pages/auth/signin.tsx
@@ -0,0 +1,38 @@
+import {
+    getProviders,
+    signIn as SignIntoProvider,
+    useSession,
+  } from "next-auth/react";
+  
+  function signIn({ providers }: any) {
+    //eslint-disable-next-line react-hooks/rules-of-hooks
+    const { data: session, status } = useSession();
+  
+    console.log(session);
+  
+    return (
+      <>
+        {Object.values(providers).map((provider: any) => (
+          <div key={provider.name}>
+            <button
+              onClick={() => SignIntoProvider(provider.id, { callbackUrl: "/" })}
+            >
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </>
+    );
+  }
+  
+  export async function getServerSideProps() {
+    const providers = await getProviders();
+  
+    return {
+      props: {
+        providers,
+      },
+    };
+  }
+  
+  export default signIn;
